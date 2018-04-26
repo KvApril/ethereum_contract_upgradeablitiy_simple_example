9c7de8f102b82078eb851d7f42d57756ab1aeb80
Unknown <joranfonjallaz@gmail.com>
2018-04-26 11:41:16 +0200

    variable name refactoring

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,30 +5,30 @@ contract TokenVersion1 {
 
     event Transfer(address _from, address _to, uint256 _value);
 
-    function balanceOf(address addr) public view returns (uint) {
-        return balances[addr];
+    function balanceOf(address _address) public view returns (uint) {
+        return balances[_address];
     }
 
-    function transfer(address to, uint256 value) public {
-        require(balances[msg.sender] >= value);
-        balances[msg.sender] -= value;
-        balances[to] += value;
-        emit Transfer(msg.sender, to, value);
+    function transfer(address _to, uint256 _value) public {
+        require(balances[msg.sender] >= _value);
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
     }
 
     // there is a bug in this function: value should not
     // be multiplied by 2
-    function mint(address to, uint256 value) public {
-        balances[to] += value * 2;
-        emit Transfer(0x0, to, value);
+    function mint(address _to, uint256 _value) public {
+        balances[_to] += _value * 2;
+        emit Transfer(0x0, _to, _value);
     }
 }
 
 contract TokenVersion2 is TokenVersion1 {
 
     // bug corrected here: multiplication by 2 removed
-    function mint(address to, uint256 value) public {
-        balances[to] += value;
-        emit Transfer(0x0, to, value);
+    function mint(address _to, uint256 _value) public {
+        balances[_to] += _value;
+        emit Transfer(0x0, _to, _value);
     }
 }
